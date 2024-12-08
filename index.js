2878011a32ed60364a7ba53f45691150b44fba15
MD Al Mamun Galib <mdalmamungalib3929@gmail.com>
2024-12-08 22:34:27 +0600

    server html page add

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/home", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
 //Database connection
 app.get("/", (req, res) => {
   res.send("Server is up and running");
